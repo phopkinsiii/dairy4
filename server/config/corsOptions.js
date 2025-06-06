97c976fc71931dc3312c5aecb94766717aeca8aa
Philip Hopkins <phopkins1757@gmail.com>
2025-06-06 13:48:55 -0400

    Refactored server.js to use a cors config file. Changed the vite base url for api end point. Ready for final deployment.

--- server/config/corsOptions.js
+++ server/config/corsOptions.js
@@ -4,6 +4,7 @@ const allowedOrigins = [
 	'https://www.blueberrydairy.com',
 	'http://localhost:5173',
 	'http://127.0.0.1:5173',
+	'https://api.blueberrydairy.com',
 ].filter(Boolean);
 
 console.log('✅ Allowed CORS Origins:', allowedOrigins);
